0cbea906b970dc715fee6a838de63201a27f61bf
Zachary Palmer <zanyzakary@gmail.com>
2018-01-08 20:25:16 -0500

    Base Game working w/o AI

--- TTT.cpp
+++ TTT.cpp
@@ -7,88 +7,77 @@
 
 int main(int argc, char *argv[])
 {
-  int rDepth;  //recursion Depth
-  int iHeight; //initial Height
+  std::string user1, user2;
+  std::cout << "Please enter the name of the first player (X's) or type AI for an automated player: ";
+  std::cin >> user1;
+  std::cout << "Please enter the name of the second player (O's) or type AI for an automated player: ";
+  std::cin >> user2;
+  gameBoard game(user1,user2);
+  while(!game.getWon()) {
+    game.draw();
+    int xMove, yMove;
+    std::cout << "Enter the X and Y coordinates of the place you want to pick: ";
+    std::cin >> xMove;
+    std::cin >> yMove;
+    if( game.getMoves()%2 == 0 ) {
+      game.pickSpot(xMove, yMove, true);
+    } else {
+      game.pickSpot(xMove, yMove, false);
+    }
+  }
+}
 
-  if( argc != 3) { //Checking to make sure the number of arguments is correct
-    std::cout << "Please Supply Two Arguments: 1.) Recursion Depth 2.) Initial Window Height" << std::endl;
-    return -1;
+gameBoard::gameBoard(std::string player1, std::string player2)
+{
+  p1 = player1;
+  p2 = player2;
+  won = false;
+  moves = 0;
+  for(int i = 0; i < 3; i++) {
+    for(int e = 0; e < 3; e++) {
+      state[i][e] = -1;
+    }
   }
-  else if(argv[1]-1 < 0) { //Checking to make sure recursion depth is greater than 1
-    std::cout << "Recursion depth must be 1 or greater." << std::endl;
-    return -2;
-  }
-  else { //output the checked arguments and store them in variables
-    rDepth = atoi(argv[1])-1;
-    iHeight = atoi(argv[2]);
-    std::cout << "Depth: " << rDepth << std::endl << "Height: " << iHeight << std::endl;
-  }
-  sf::RenderWindow window(sf::VideoMode(iHeight,iHeight), "Original"); //display window
+}
 
-  while (window.isOpen()) {
-    sf::Event event;
-    while(window.pollEvent(event)) {
-      if(event.type == sf::Event::Closed)
-      window.close();
-    }
-    recDiamond rDiamond(rDepth,(iHeight/2),(iHeight/2),(iHeight/2)); //construct original sierpinski triangle
-    window.clear(sf::Color::White); //set background color for window
-    window.draw(rDiamond); //draw sierpinski triangle (recursivly creats and draws triangles until depth is 0)
-    window.display();
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-      //if the up arrow key is pressed increase recursion by 1;
-      if(rDepth < 10) {
-        rDepth++;
-        std::cout << "Recursion level: " << rDepth << std::endl;
+void gameBoard::draw()
+{
+  for(int i = 0; i < 100; i++) {
+    std::cout << std::endl;
+  }
+
+  for(int i = 0; i < 3; i++) {
+    for(int e = 0; e < 3; e++) {
+      if(state[i][e] == 0) {
+        std::cout << " O ";
+      } else
+      if(state[i][e] == 1) {
+        std::cout << " X ";
+      } else {
+        std::cout << "   ";
+      }
+      if(e < 2) {
+        std::cout << "|";
       }
     }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-      //if the down arrow key is pressed decrease recurstion by 1
-      if(rDepth > 0) {
-        rDepth--;
-        std::cout << "Recursion level: " << rDepth << std::endl;
-      }
-      else {
-        std::cout << "Recursion cannot be less than 1" << std::endl;
-      }
+    if(i < 2) {
+      std::cout << "\n___|___|___\n";
+    } else {
+      std::cout << "\n   |   |\n\n";
     }
   }
-  return 0;
 }
 
 
-recDiamond::recDiamond(int d, double X, double Y, double S)
+// allows the user/AI to pick a spot in the tic tac toe board based on the x and y
+// coordinate if XorO is false then the player is o's and if its true the player
+// is x's
+void gameBoard::pickSpot(int x, int y, bool XorO)
 {
-  x = X;
-  y = Y;
-  s = S;
-  depth = d;
-  S2 = (s*sqrt(2));
-  diamond.setPointCount(4);
-  if(d%2==0) {
-    diamond.setFillColor(sf::Color::Black);
-  } else {
-    diamond.setFillColor(sf::Color::Red);
+  int marker = 0;
+  if(XorO){
+    marker = 1;
   }
-  diamond.setPoint(0,sf::Vector2f(x-(s/2),  y));
-  diamond.setPoint(2,sf::Vector2f(x+(s/2),      y));
-  diamond.setPoint(1,sf::Vector2f(x,      y+(s/2)));
-  diamond.setPoint(3,sf::Vector2f(x,      y-(s/2)));
-
-}
-
-void recDiamond::draw(sf::RenderTarget &target, sf::RenderStates states) const
-{
-  target.draw(diamond,states);
-  if(depth >0) { //construct the three children triangles(depth, two vertexes, side length)
-    recDiamond a(depth-1,  x-(s)*.5,     y+s*.5,  s*.5);
-    recDiamond b(depth-1,  x+(s)*.5,     y+s*.5,  s*.5);
-    recDiamond c(depth-1,  x-(s)*.5,     y-s*.5,  s*.5);
-    recDiamond d(depth-1,  x+(s)*.5,     y-s*.5,  s*.5);
-    //call the draw method on the children
-    target.draw(a);
-    target.draw(b);
-    target.draw(c);
-    target.draw(d);
-  }
+  state[x][y] = marker;
+  moves++;
 }
--- TTT.hpp
+++ TTT.hpp
@@ -3,15 +3,46 @@
 #ifndef __TTT_HPP_INCLUDED
 #define __TTT_HPP_INCLUDED
 
+#include <string>
+#include <stdlib.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-class recDiamond : public sf::Drawable
+//this class is the game of tic tac toe
+class gameBoard
 {
 public:
 
+// constructor which takes the names of the players
+gameBoard(std::string player1, std::string player2);
+
+// draws the current state of the game into the terminal (will be implemented as gui)
+void draw();
+
+
+// allows the user/AI to pick a spot in the tic tac toe board based on the x and y
+// coordinate if XorO is false then the player is o's and if its true the player
+// is x's
+void pickSpot(int x, int y, bool XorO);
+
+bool getWon() { return won; }
+int getMoves() { return moves; }
+
 private:
 
+// this is a 2 dimensional array of integers which defines the current state of the game
+// a -1 indicates that the space is empty, a 1 indicates that an X is there, a 0 indicates
+// that the space has a (you guessed it) zero in it
+int state [3][3];
+
+// set to true once the game is won
+bool won;
+
+// strings for the user's names
+std::string p1, p2;
+
+// counter for the number of moves it takes for a winner to emerge
+int moves;
 };
 
 
